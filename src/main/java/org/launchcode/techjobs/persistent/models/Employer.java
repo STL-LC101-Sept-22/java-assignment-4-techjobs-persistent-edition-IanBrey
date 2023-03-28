d3394bf984bb8c125672b3a43eb614a4516c0dcb
Ian Brey <ianbrey@Ians-iMac.local>
2023-03-27 17:19:04 -0700

    Completed tasks 1-2. Having an odd bean error but will try to continue working without being able to test

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,26 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @Size(max = 80)
+    @NotNull
+    public String location;
 
+    public Employer() {}
+
+    public Employer(String location) {
+        this.location = location;
+    }
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
