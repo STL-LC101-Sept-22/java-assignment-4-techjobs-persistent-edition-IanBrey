f14d7909f0744035266d0c12b1d797f065b03799
Ian Brey <ianbrey@Ians-iMac.local>
2023-03-30 16:19:33 -0700

    Completed assignment

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -11,11 +15,18 @@ public class Employer extends AbstractEntity {
     @NotNull
     public String location;
 
+    @OneToMany(mappedBy = "employer")
+//    @JoinColumn
+    private final List<Job> jobs = new ArrayList<>();
+
     public Employer() {}
 
     public Employer(String location) {
         this.location = location;
     }
+
+    public List<Job> getJobs() { return jobs; }
+
     public String getLocation() {
         return location;
     }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,15 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
     public String description;
 
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs;
+
     public Skill () {}
 
-    public Skill (String description) {
+    public Skill (String description, List<Job> jobs) {
+        this.jobs = jobs;
         this.description = description;
     }
 
@@ -19,5 +25,13 @@ public class Skill extends AbstractEntity {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
     }
 }
